cf9048775db20f8be6cb5e1d03762f471edbbd00
Anahif099 <rtsccp@gmail.com>
2025-12-22 11:33:18 +0530

    `Removed unused imports from Blog.tsx`

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,6 +1,4 @@
 import BlogHero from "@/components/BlogHero";
-import CreatorHero from "@/components/CreatorHero";
-import FlipbookViewer from "@/components/FlipbookViewer";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
