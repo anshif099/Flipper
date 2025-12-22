e3eb6c29325b4eaa61e0a07c627e019177be464c
Anahif099 <rtsccp@gmail.com>
2025-12-22 11:32:09 +0530

    `Added Blog page and route to App component`

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Creator from "./pages/Creator";
+import Blog from "./pages/Blog";
 
 
 const queryClient = new QueryClient();
@@ -20,6 +21,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="Creator" element={<Creator/>} />
+          <Route path="Blog" element={<Blog/>} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
---
+++ src/pages/Blog.tsx
@@ -0,0 +1,18 @@
+import BlogHero from "@/components/BlogHero";
+import CreatorHero from "@/components/CreatorHero";
+import FlipbookViewer from "@/components/FlipbookViewer";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+
+const Index = () => {
+  return (
+    <>
+      <Header />
+      <BlogHero />
+      <Footer />
+    </>
+  );
+};
+
+export default Index;
