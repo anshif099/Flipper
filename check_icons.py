eef3d2d29690ed41cc0e28074cee603335af7e4e
anshif099 <153600093+anshif099@users.noreply.github.com>
2025-12-24 15:12:32 +0530

    Delete check_icons.py

--- check_icons.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-icons = ['public/icon-192.png', 'public/icon-512.png']
-for icon in icons:
-    try:
-        img = Image.open(icon)
-        print(f"{icon}: Width: {img.width}, Height: {img.height}")
-    except Exception as e:
-        print(f"Error with {icon}: {e}")
